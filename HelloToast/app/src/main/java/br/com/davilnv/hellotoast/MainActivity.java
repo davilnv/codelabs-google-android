937be1f964883e0b7e18d5cf6e4588f2c95e377a
davilnv <davilima871@gmail.com>
2023-08-22 14:36:45 -0300

    LinearLayout

--- HelloToast/app/src/main/java/br/com/davilnv/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/br/com/davilnv/hellotoast/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.davilnv.hellotoast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
